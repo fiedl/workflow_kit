75da5e3f2d330a5570d5fc6095db68d033ae9c18
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-21 17:29:42 +0200

    solving namespace issues. test workflow now executable.

--- app/models/workflow_kit/step.rb
+++ app/models/workflow_kit/step.rb
@@ -1,4 +1,7 @@
 module WorkflowKit
+
+  require 'workflow_kit/brick'
+  
   class Step < ActiveRecord::Base
 
     attr_accessible :sequence_index, :brick_name
@@ -6,14 +9,15 @@ module WorkflowKit
     belongs_to :workflow
     has_many :parameters, dependent: :destroy, as: :parameterable
 
-    def execute( params )
-      params = params.merge( Parameter.to_hash( self.parameters ) )
+    def execute( params = {} )
+      params = {} unless params
+      params = params.merge( Parameter.to_hash( self.parameters ) ) if self.parameters.count > 0
       self.brick.execute( params ) if self.brick
     end
 
     def brick
       unless @brick
-        class_name = self.brick_name
+        class_name = "WorkflowKit::#{self.brick_name}" if self.brick_name
         @brick = class_name.constantize.new if class_name
       end
       return @brick
--- app/models/workflow_kit/workflow.rb
+++ app/models/workflow_kit/workflow.rb
@@ -1,3 +1,4 @@
+
 module WorkflowKit
   class Workflow < ActiveRecord::Base
     attr_accessible :description, :name
@@ -5,10 +6,11 @@ module WorkflowKit
     has_many :steps, dependent: :destroy, order: :sequence_index
     has_many :parameters, as: :parameterable, dependent: :destroy
 
-    def execute
-      parameter_hash = Parameter.to_hash( self.parameters )
+    def execute( params = {} )
+      params = {} unless params
+      params = params.merge( Parameter.to_hash( self.parameters ) ) if self.parameters.count > 0
       self.steps.collect do |step|
-        step.execute( parameter_hash )
+        step.execute( params )
       end
     end
 
