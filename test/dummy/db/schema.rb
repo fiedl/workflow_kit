255bc2d33abe51aeedb0db2aeb2968544023cf40
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-22 01:56:32 +0200

    message logger for the demo app to demonstrate the execution of workflows.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120721140613) do
+ActiveRecord::Schema.define(:version => 20120721225047) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "text"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "workflow_kit_parameters", :force => true do |t|
     t.string   "key"
