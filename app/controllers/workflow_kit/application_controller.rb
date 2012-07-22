96eb6870e0b6b101939f1e5727aed2a5dd3d2205
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-22 15:55:44 +0200

    making the main app's helper methods available, in order to be able to use the main app's layout. See: app/controller/workflow_kit/application_controller.rb

--- app/controllers/workflow_kit/application_controller.rb
+++ app/controllers/workflow_kit/application_controller.rb
@@ -1,5 +1,26 @@
 module WorkflowKit
-  class ApplicationController < ActionController::Base
-    layout 'application'
+
+  class ApplicationController < ::ApplicationController
+
+    #
+    # What is this controller inheritance doing?
+    # This makes the class WorkflowKit::ApplicationController inherit from the main
+    # application's ApplicationController. 
+    # Therefore, all helper methods of the main app will be available in the engine.
+    # That is necessary if you want to use the main app's layout, because this
+    # layout will most likely use some helper methods.
+    # 
+    # To restore the isolation from the main app's ApplicationController, 
+    # one could use the inheritance originally used by the `rails g plugin --mountable`,
+    # which is:
+    # 
+    #    class ApplicationController < ActionController::Base
+    # 
+    # For further information, I recommend having a look at this extremely 
+    # helpful article:
+    # 
+    #    http://bibwild.wordpress.com/2012/05/10/the-semi-isolated-rails-engine/
+    #
+
   end
 end
--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def helpers_of_the_main_app_can_be_called?
+    true
+  end
+
 end
