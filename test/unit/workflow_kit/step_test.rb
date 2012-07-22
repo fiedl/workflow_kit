8c53e70c600cd3b3f81d6c22e33c1467bcbe7147
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-22 02:14:07 +0200

    removing empty tests

--- test/unit/workflow_kit/step_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module WorkflowKit
-  class StepTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
--- test/workflow_kit_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkflowKitTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, WorkflowKit
-  end
-end
